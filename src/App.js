e6b08c0d63b3c13490ea2ec614e5d89ff9d24ddc
Random Guy <leekhoi5319990@gmail.com>
2021-10-14 22:07:15 +0700

    .

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { UserRepositories } from "./UserRepositories";
 import { RepositoryReadme } from "./RepositoryReadme";
 
 export default function App() {
-  const [login, setLogin] = useState("helpme-pls");
-  const [repo, setRepo] = useState("waterfallXHR");
+  const [login, setLogin] = useState("moonhighway");
+  const [repo, setRepo] = useState("learning-react");
   return (
     <>
       <SearchUser value={login} onSearch={setLogin} />

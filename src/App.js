ab4e3cc6d6916f9d8d9e58474a4d3570aa9f54fb
Random Guy <leekhoi5319990@gmail.com>
2021-10-14 22:45:30 +0700

    Cancelling requests

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ import { RepositoryReadme } from "./RepositoryReadme";
 export default function App() {
   const [login, setLogin] = useState();
   const [repo, setRepo] = useState();
+
+  const handleSearch = login => {
+    if (login) return setLogin(login);
+      setLogin("");
+      setRepo("");
+  };
+
+  if (!login)
+    return (
+      <SearchForm value={login} onSearch={handleSearch} />
+    );
+
   return (
     <>
-      <SearchUser value={login} onSearch={setLogin} />
+      <SearchUser value={login} onSearch={handleSearch} />
       {login && <GitHubUser login={login} />}
       {login && <UserRepositories login={login} repo={repo} selectedRepo={setRepo} />}
       {login && repo && <RepositoryReadme login={login} repo={repo} />}
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,20 +1,37 @@
 import { useState, useEffect, useCallback, useMemo } from "react";
 
+export function useMountedRef() {
+  const mounted = useRef(false);
+  useEffect(() => {
+    mounted.current = true;
+    return () => (mounted.current = false);
+  });
+  return mounted;
+}
+
 export function useFetch(uri) {
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
+  const mounted = useMountedRef();
 
   useEffect(() => {
     if (!uri) return;
     fetch(uri)
+      .then(data => {
+        if (!mounted.current) throw new Error("component is not mounted");
+        return data;
+      })
       .then((data) => data.json())
       .then(setData) // (res) => setData(res), taking the result from the previous .then()
       .then(() => setLoading(false)) // setLoading(false) has to be put in a callback function so that it'll be executed AFTER the re-render triggered by setData
       // .then(setLoading): why tf this works, how does it know that we need to pass in {false} ?? -> it doesn't. It will pass the result of setData to the argument of setLoading. Which is very likely undefined. So loading will be set to undefined, which is FALSY (calling Boolean(undefined) === false)
       // .then(setLoading(false)): why tf this DOESN'T work ??? -> .then() needs a FUNCTION. You are passing a statement here.
 
-      .catch(setError);
+      .catch(error => {
+        if (!mounted.current) return;
+        setError(error);
+      })
   }, [uri]);
   return {
     // hook's return values as an object doesn't have to be strict in order when invoked
